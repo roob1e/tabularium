8925701c428f471ffc9afd32241bf3c29c9fe772
Roob!e <80145015+roob1e@users.noreply.github.com>
2025-09-10 21:34:14 +0300

    Улучшил код

--- backend/src/main/java/com/roobie/backend/repository/GroupRepository.java
+++ backend/src/main/java/com/roobie/backend/repository/GroupRepository.java
@@ -4,10 +4,9 @@ import com.roobie.backend.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Integer> {
-    public Optional<Group> findByName(String name);
+    Optional<Group> findByName(String name);
 }
