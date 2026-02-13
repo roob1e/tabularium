7e4a7390fc304639a107c746c39fdfb0005241b2
assxmblxr <danilenkoa4465@gmail.com>
2026-02-13 20:47:17 +0300

    test

--- backend/src/main/java/com/assxmblxr/backend/repository/GradeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assxmblxr.backend.repository;
-
-import com.assxmblxr.backend.entity.Grade;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GradeRepository  extends JpaRepository<Grade, Long> {
-}
